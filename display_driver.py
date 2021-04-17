0a75b4d68c3b4617517264edadce4cebd0fbe361
Timothy Bilik <timothy@bilikfamily.com>
2021-04-17 16:36:08 -0400

    fixes

--- display_driver.py
+++ display_driver.py
@@ -9,6 +9,7 @@ import RPi.GPIO as GPIO
 
 # determines whether the buzzer should be used
 alarm = True
+obdOn = False
 
 # define pin values
 buttonPin = 19
@@ -19,7 +20,7 @@ currentSpeed = 0
 # set pin states
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttonPin, GPIO.IN)
 GPIO.setup(buzzerPin, GPIO.OUT)
 buzzer = GPIO.PWM(buzzerPin, 2000)
 
@@ -35,9 +36,14 @@ def toggle_alarm(channel):
     global buzzer
     global display
     global currentSpeed
-    display[0] = currentSpeed >= 90 ? 9 : currentSpeed / 10 
+    if currentSpeed >= 90:
+        display[0] = "9"
+    else:
+        display[0] = str(currentSpeed // 10) 
     buzzer.stop()
     alarm = not alarm
+    if alarm:
+        display[0] = "."
 
 GPIO.add_event_detect(buttonPin, GPIO.RISING, callback=toggle_alarm, bouncetime=200)
 
@@ -66,6 +72,7 @@ with open("display_fifo") as fifo:
         if data == "":
             continue
         if data[0] == "A":
+            obdOn = True
             if len(data) == 2:
                 display[0] = "0"
                 display[1] = data[1]
@@ -75,45 +82,70 @@ with open("display_fifo") as fifo:
             else:
                 display[0] = "9"
                 display[1] = "9"
+            display[1] = "."
             if alarm:
                 display[0] = "."
+            currentSpeed = int(data[1:])
+        if data[0] == "C":
+            if currentSpeed >= 99:
+                display[1] = "9"
+            else:
+                display[1] = str(currentSpeed % 10)
+            obdOn = False
         if data[0] == "B":
             if data[1:] == "ten":
                 display[2] = "1"
                 display[3] = "0"
+                speedLimit = 10
             if data[1:] == "fifteen":
                 display[2] = "1"
                 display[3] = "5"
+                speedLimit = 15
             if data[1:] == "twenty":
                 display[2] = "2"
                 display[3] = "0"
+                speedLimit = 20
             if data[1:] == "twentyfive":
                 display[2] = "2"
                 display[3] = "5"
+                speedLimit = 25
             if data[1:] == "thirty":
                 display[2] = "3"
                 display[3] = "0"
+                speedLimit = 30
             if data[1:] == "thirtyfive":
                 display[2] = "3"
                 display[3] = "5"
+                speedLimit = 35
             if data[1:] == "forty":
                 display[2] = "4"
                 display[3] = "0"
+                speedLimit = 40
             if data[1:] == "fortyfive":
                 display[2] = "4"
                 display[3] = "5"
+                speedLimit = 45
             if data[1:] == "fifty":
                 display[2] = "5"
                 display[3] = "0"
+                speedLimit = 50
             if data[1:] == "fiftyfive":
                 display[2] = "5"
                 display[3] = "5"
+                speedLimit = 55
             if data[1:] == "sixty":
                 display[2] = "6"
                 display[3] = "0"
+                speedLimit = 60
             if data[1:] == "sixtyfive":
                 display[2] = "6"
                 display[3] = "5"
+                speedLimit = 65
             if data[1:] == "seventy":
                 display[2] = "7"
                 display[3] = "0"
+                speedLimit = 70
+        if alarm and obdOn and speedLimit != 0 and currentSpeed - speedLimit >= 10:
+            buzzer.start(90)
+        else:
+            buzzer.stop()
---
+++ obdii.py
@@ -0,0 +1,33 @@
+import obd
+from obd import OBDStatus
+import time
+import math
+
+def retrieveData(pid):
+    return connection.query(obd.commands[pid]).value
+
+# connect to ELM327 OBD-II interface chip
+while True:
+    try:
+        connection = obd.Async(fast=False,timeout=30)
+        if connection.status() == OBDStatus.CAR_CONNECTED:
+            break
+    except:
+        # this code runs if the connection fails in some way
+        print("Connection failed. Trying again ...") # this is for the log file
+
+connection.watch(obd.commands["SPEED"])
+# start connection of interface chip with vehicle
+connection.start()
+# approximately 2 seconds needed to stabilize connection
+time.sleep(2)
+
+while true:
+    while connection.status() != OBDStatus.CAR_CONNECTED:
+        with open("/home/tbilik/display_fifo","w") as fp:
+            fp.write("C")
+        time.sleep(1)
+    with open("/home/tbilik/display_fifo","w") as fp:
+        speed = retrieveData("SPEED").to("mph").magnitude
+        fp.write("A%d\n" % (speed,))
+    time.sleep(1)
